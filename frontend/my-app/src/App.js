4e4f00c1807f7de06c2abf3503865b4ea62167d7
Anson Wan <anson.hy.wan@gmail.com>
2024-02-13 15:23:50 -0500

    Initial frontend commit

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/NavBar/navbar";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar />
+    </div>
+  );
+}
+
+export default App;
